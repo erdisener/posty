761ba3324435b8e61c0d973cf49d3c2d5cf6d2f0
Erdi Sener <erdi.sener@sisu9.com>
2021-11-07 16:49:53 +0300

    add remember checkbox

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,19 +7,28 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware(['guest']);
+    }
+
+
     public function index() {
 
         return view('auth.login');
     }
 
     public function store(Request $request) {
+
+
         $this->validate($request, [
            
             'email' => 'required|email',
             'password' => 'required',
         ]);
 
-        if(!auth()->attempt($request->only('email', 'password'))) {
+        if(!auth()->attempt($request->only('email', 'password'),$request->remember)) {
             return back()->with('status','Geçersiz e-posta ya da parola!');
         }
         
