213fadffe2be4502354ec236df1ddf42bb0e5065
Erdi Sener <erdi.sener@sisu9.com>
2021-11-08 10:58:18 +0300

    beğen, beğenme butonları ve metodları eklendi

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -26,8 +26,30 @@
                     <div class="mb-4">
                         <a href="" class="font-bold"> {{$post->user->name}} </a> <span class="text-gray-600 text-sm"> {{$post->created_at->diffForHumans() }} </span>
                         <p class="mb-2"> {{ $post->body }} </p>
+
+                        <div class="flex item-center">
+                            @if (!$post->likedBy(auth()->user()))
+                            <form action="{{ route('posts.likes', $post) }}" method="POST" class="mr-1">
+                                @csrf
+                                <button type="submit" class="text-blue-500">Beğen</button>
+                            </form>
+                            @else
+                            <form action="{{ route('posts.likes', $post) }}" method="POST" class="mr-1">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="text-blue-500">Beğenme</button>
+                            </form>
+                            @endif
+                            
+                            <span>{{ $post->likes->count() }} Beğeni</span>
+                        </div>
+
+
                     </div>
                 @endforeach
+
+                {{ $posts->links() }}
+
             @else
                 <p>Henüz hiçbir gönderi bulunmamaktadır.</p>
             @endif
